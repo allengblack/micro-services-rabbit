01bb7226f3ad12202ba57486a208230a1558fa01
Gbolahan Allen <allengblack@gmail.com>
2021-09-28 09:26:13 +0100

    remove weather forecast controller

---
+++ Subscriber.NET/Controllers/PeopleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Subscriber.NET.Models;
+
+namespace Subscriber.NET.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PeopleController : ControllerBase
+    {
+        private readonly ILogger<PeopleController> _logger;
+
+        public PeopleController(ILogger<PeopleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Person
+            {
+                Age = 50,
+                Name = "Femi Joseph",
+                Address = "Kano, Yo Mama"
+            })
+            .ToArray();
+        }
+    }
+}
--- Subscriber.NET/Models/Person.cs
+++ Subscriber.NET/Models/Person.cs
@@ -3,5 +3,7 @@ namespace Subscriber.NET.Models
     public class Person
     {
         public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
     }
 }
